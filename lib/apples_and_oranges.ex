ea2f5cd6f0cafa343b70e9f2434a75613aee13d4
Michael Berkowitz <michael.berkowitz@gmail.com>
2016-01-15 18:13:56 -0500

    Move main functionality into ApplesAndOranges

--- lib/apples_and_oranges.ex
+++ lib/apples_and_oranges.ex
@@ -1,2 +1,60 @@
 defmodule ApplesAndOranges do
+  alias ApplesAndOranges.ScreenshotSet
+  alias ApplesAndOranges.ImageMatcher
+
+  use Hound.Helpers
+
+  defmacro __using__(_opts) do
+    quote do
+      import ApplesAndOranges
+    end
+  end
+
+  @screenshot_root_dir ScreenshotSet.screens_root
+
+  def it_looks_like(context, name \\ nil) do
+    path = case name do
+             nil -> build_path(context)
+             _ -> build_path(context, name)
+           end
+    it_looks_right(path)
+  end
+
+  def it_looks_right(path) do
+    set = ensure_set_directory(path)
+    save_screenshot(set)
+    case ImageMatcher.matches?(set) do
+      {:ok, _} ->
+        Path.wildcard(set.path <> "/{current,diff}.png") |> Enum.map &File.rm/1
+        true
+      {:error, _} -> false
+      {:raise, msg} -> raise msg
+    end
+  end
+
+  defp ensure_set_directory(path) do
+    set = %ScreenshotSet{path: path}
+    ScreenshotSet.ensure_directory(set)
+    set
+  end
+
+  defp save_screenshot(nil), do: nil
+  defp save_screenshot(set) do
+    take_screenshot(set.path <> "/current.png")
+  end
+
+  defp build_path(nil), do: nil
+  defp build_path(context), do: build_path(context, Atom.to_string(context.test))
+  defp build_path(context, test_name) do
+    [@screenshot_root_dir, base_case_name(context.case), test_name]
+    |> Enum.join("/")
+    |> String.replace(" ", "-")
+    |> String.downcase
+  end
+
+  defp base_case_name(case_name) do
+    Atom.to_string(case_name)
+    |> String.split(".")
+    |> List.last
+  end
 end
--- test/apples_and_oranges_test.exs
+++ test/apples_and_oranges_test.exs
@@ -1,8 +1,64 @@
 defmodule ApplesAndOrangesTest do
   use ExUnit.Case
-  doctest ApplesAndOranges
+  use Hound.Helpers
+  use ApplesAndOranges.TestHelpers
+  use ApplesAndOranges
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  alias ApplesAndOranges.ScreenshotSet
+
+  hound_session
+
+  @accepted_set build_set("test_helper", "accepted_test")
+  @empty_set build_set("test_helper", "empty_test")
+
+  test "it_looks_right returns true if 'current' and 'accepted' screenshots match" do
+    navigate_to("http://localhost:9090/index.html")
+    assert it_looks_right(@accepted_set.path)
+  end
+
+  test "it_looks_right deletes the current screenshot if it matches the accepted screenshot" do
+    navigate_to("http://localhost:9090/index.html")
+    assert it_looks_right(@accepted_set.path)
+    refute ScreenshotSet.current_image(@accepted_set)
+  end
+
+  test "it_looks_right returns false if 'current' and 'accepted' screenshots do not match" do
+    refute it_looks_right(@accepted_set.path)
+  end
+
+  test "it_looks_right retains ther 'current' image if it does not match the existing 'accepted'" do
+    refute it_looks_right(@accepted_set.path)
+    assert ScreenshotSet.current_image(@accepted_set)
+  end
+
+  test "it_looks_right raises an error if there is no accepted screenshot" do
+    navigate_to("http://localhost:9090/index.html")
+    assert_raise RuntimeError, fn ->
+      it_looks_right(@empty_set.path)
+    end
+  end
+
+  test "build_path creates the correct directory structure when no test name is passed in", context do
+    navigate_to("http://localhost:9090/index.html")
+    assert_raise RuntimeError, fn ->
+      it_looks_like(context)
+    end
+    assert ScreenshotSet.current?(
+      %ScreenshotSet{
+        path: "priv/static/screens/applesandorangestest/test-build_path-creates-the-correct-directory-structure-when-no-test-name-is-passed-in"
+      }
+    )
+  end
+
+  test "build_path creates the correct directory structure", context do
+    navigate_to("http://localhost:9090/index.html")
+    assert_raise RuntimeError, fn ->
+      it_looks_like(context, "new test")
+    end
+    assert ScreenshotSet.current?(
+      %ScreenshotSet{
+        path: "priv/static/screens/applesandorangestest/new-test"
+      }
+    )
   end
 end
